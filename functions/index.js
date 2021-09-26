4ed3ccad32639f4c44cb31a12b054d73a7e8f747
Christopher Ortega <A01634500@itesm.mx>
2021-09-26 13:52:30 -0600

    last push to PR

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
 const admin = require('./firebase')
 const functions = require("firebase-functions");
-
+const logger = require('morgan')('dev');
 const express = require("express");
-
 const app = express();
 
 app.use(express.json());
@@ -16,11 +15,38 @@ app.use((req, res, next) => {
     ][0]);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+}); 
+
+const firestore = admin.firestore();
 
 app.post("/project", (req, res) => {
-    console.log(req.body.data)
-    
+    firestore.collection("projects").add(req.body)
+    .then(doc => {
+        return res.status(201).send({
+            "success": true,
+            "message": "Proyecto registrado exitosamente"
+        })
+
+    })
+    .catch(er => {
+        console.error(er)
+        return res.status(401).send(er)
+    })
+})
+
+app.get("/all", (req, res) => {
+    firestore.collection("projects").get()
+    .then(query => {
+        projects = {}
+        query.forEach(doc => {
+            projects[doc.id] = doc.data();
+        })
+        return res.status(200).send(projects);
+    })
+    .catch(er => {
+        console.error(er)
+        return res.status(400).send(er)
+    })
 })
 
 exports.api = functions.region("us-central1").https.onRequest(app);

02517f5805603d790e53356cd15c6fa6887f747a
EsquizoDEV <luismtzesq@gmail.com>
2021-11-12 13:36:38 -0600

    simple changes

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,11 +1,9 @@
 const admin = require('firebase-admin');
 
-require('dotenv').config({
-    path: "./.env"
-})
-
 const config = require('./hervana-af5fd-firebase-adminsdk-a1ito-ed574cb4ce.json');
 
-admin.initializeApp(config);
+admin.initializeApp({
+    credential: admin.credential.cert(config)
+});
 
 module.exports = admin;
--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", [
         "http://localhost:3000",
-        "https://hevanagroup.net/"
+        "https://hevanagroup.net"
     ][1]);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
